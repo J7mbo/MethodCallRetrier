df19ee974a031add2e894c12d2db8a5ac2ba9fc2
James Mallison <james.mallison@sixt.com>
2019-03-15 15:56:03 +0100

    [#19] - Retrier interface now matches struct as expected

--- Retrier.go
+++ Retrier.go
@@ -4,9 +4,7 @@ import "reflect"
 
 /* Represents an object capable of retrying a call on an object X times after receiving an error. */
 type Retrier interface {
-	ExecuteWithRetry(
-		maxRetries int64, waitTime int64, object interface{}, methodName string, args ...interface{},
-	) ([]reflect.Value, error)
+	ExecuteWithRetry(object interface{}, methodName string, args ...interface{}) ([]reflect.Value, []error)
 
 	ExecuteFuncWithRetry(function func() error) []error
 }
